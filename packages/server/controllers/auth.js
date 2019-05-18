283e69e0a98a3612bdf944b30cef4bfbad7e4387
davitmuradyan <davitmuradyan5@gmail.com>
2019-05-18 22:16:30 +0400

    add try catch block and fix user profile update bug

--- packages/server/controllers/auth.js
+++ packages/server/controllers/auth.js
@@ -11,146 +11,184 @@ const {
 } = require('../errors/UserErrors');
 
 module.exports.login = async (req, res, next) => {
-  const candidate = await User.findOne({username: req.body.username});
+  try {
+    const candidate = await User.findOne({username: req.body.username});
 
-  if (!candidate) {
-    return next(new UserNotFoundError(req.body.username));
-  }
-  if (!bcryptjs.compareSync(req.body.password, candidate.password)) {
-    return next(new IncorrectCredentialsError());
-  }
-  if (candidate.verified === false) {
-    return next(new AccountIsNotVerifiedError());
-  }
-  const accessToken = jwt.sign({
-    username: candidate.username,
-    _id: candidate._id,
-    type: candidate.type,
-  }, JWT_SECRET_KEY, { expiresIn: "24 hours"});
+    if (!candidate) {
+      return next(new UserNotFoundError(req.body.username));
+    }
+    if (!bcryptjs.compareSync(req.body.password, candidate.password)) {
+      return next(new IncorrectCredentialsError());
+    }
+    if (candidate.verified === false) {
+      return next(new AccountIsNotVerifiedError());
+    }
+    const accessToken = jwt.sign({
+      username: candidate.username,
+      _id: candidate._id,
+      type: candidate.type,
+    }, JWT_SECRET_KEY, { expiresIn: "24 hours"});
 
-  res.status(200).json({
-    accessToken,
-    userEmail: candidate.email,
-    username: candidate.username,
-    firstname: candidate.firstname,
-    lastname: candidate.lastname,
-    id: candidate._id,
-    imgSrc: candidate.imgSrc,
-    type: candidate.type,
-  });
+    res.status(200).json({
+      accessToken,
+      userEmail: candidate.email,
+      username: candidate.username,
+      firstname: candidate.firstname,
+      lastname: candidate.lastname,
+      id: candidate._id,
+      imgSrc: candidate.imgSrc,
+      type: candidate.type,
+    });
+    return next(null);
+  } catch (error) {
+    return next(error);
+  }
 };
 
 module.exports.register = async (req, res, next) => {
-  const candidate = await User.findOne({$or: [
-    {username: req.body.username},
-    {email: req.body.email}
-  ]});
+  try {
+    const candidate = await User.findOne({$or: [
+        {username: req.body.username},
+        {email: req.body.email}
+      ]});
 
-  if (candidate) {
-    return next(new UserAlreadyExistsError(req.body.username));
+    if (candidate) {
+      return next(new UserAlreadyExistsError(req.body.username));
+    }
+    const user = await new User({
+      authToken: mailer.verifyURL,
+      username: req.body.username,
+      email: req.body.email,
+      password: bcryptjs.hashSync(req.body.password, 4),
+      firstname: req.body.firstname,
+      lastname: req.body.lastname
+    }).save();
+
+    await mailer.sendMail(req.body.email);
+    res.status(201).json(user);
+    return next(null);
+  } catch (error) {
+    return next(error);
   }
-  const user = await new User({
-    authToken: mailer.verifyURL,
-    username: req.body.username,
-    email: req.body.email,
-    password: bcryptjs.hashSync(req.body.password, 4),
-    firstname: req.body.firstname,
-    lastname: req.body.lastname
-  }).save();
-
-  await mailer.sendMail(req.body.email);
-  res.status(201).json(user);
-  return next(null);
 };
 
 module.exports.verifyEmail = async (req, res, next) => {
-  const user = await User.findOneAndUpdate(
-    {authToken: req.params.authToken},
-    req.body,
-  );
-  const accessToken = jwt.sign({
-    username: user.username
-  }, JWT_SECRET_KEY, { expiresIn: "6 hours"});
-  const newUser = {
-    accessToken,
-    userEmail: user.email,
-    username: user.username,
-    firstname: user.firstname,
-    lastname: user.lastname,
-    id: user._id
-  };
+  try {
+    const user = await User.findOneAndUpdate(
+      {authToken: req.params.authToken},
+      req.body,
+    );
+    const accessToken = jwt.sign({
+      username: user.username
+    }, JWT_SECRET_KEY, { expiresIn: "6 hours"});
+    const newUser = {
+      accessToken,
+      userEmail: user.email,
+      username: user.username,
+      firstname: user.firstname,
+      lastname: user.lastname,
+      id: user._id
+    };
 
-  res.status(200).json(newUser);
-  return next(null);
+    res.status(200).json(newUser);
+    return next(null);
+  } catch (error) {
+    return next(error);
+  }
 };
 
 module.exports.checkEmail = async (req, res, next) => {
-  const candidate = await User.findOne({email: req.body.email});
+  try {
+    const candidate = await User.findOne({email: req.body.email});
 
-  if (candidate) {
-    return next(new UserAlreadyExistsError(req.body.email));
+    if (candidate) {
+      return next(new UserAlreadyExistsError(req.body.email));
+    }
+    res.status(200).json({
+      message: 'Free email'
+    });
+    return next(null);
+  } catch (error) {
+    return next(error);
   }
-  res.status(200).json({
-    message: 'Free email'
-  });
-  return next(null);
 };
 
 module.exports.checkUsername = async (req, res, next) => {
-  const candidate = await User.findOne({username: req.body.username});
+  try {
+    const candidate = await User.findOne({username: req.body.username});
 
-  if (candidate) {
-    return next(new UserAlreadyExistsError(req.body.username));
+    if (candidate) {
+      return next(new UserAlreadyExistsError(req.body.username));
+    }
+    res.status(200).json({
+      message: 'Free username'
+    });
+    return next(null);
+  } catch (error) {
+    return next(error);
   }
-  res.status(200).json({
-    message: 'Free username'
-  });
-  return next(null);
 };
 
 module.exports.editprofile = async (req, res, next) => {
-  const user = await User.findById(req.user._id);
-  const updatedUser = await User.findOneAndUpdate(req.user._id, {
-    firstname: req.body.firstname,
-    lastname: req.body.lastname,
-    username: req.body.username,
-    imgSrc: req.file
-      ? req.file.path
-      : user.imgSrc
-  }, { new: true });
+  try {
+    const user = await User.findById(req.user._id);
+    const updatedUser = await User.findOneAndUpdate({_id: req.user._id}, {
+      firstname: req.body.firstname,
+      lastname: req.body.lastname,
+      username: req.body.username,
+      type: user.type,
+      imgSrc: req.file
+        ? req.file.path
+        : user.imgSrc
+    }, { new: true });
 
-  res.status(200).json(updatedUser);
-  return next(null);
+    res.status(200).json(updatedUser);
+    return next(null);
+  } catch (error) {
+    return next(error);
+  }
 };
 
 module.exports.contact = async (req, res, next) => {
-  const data = {
-    from: req.body.contactFormEmail,
-    to: '82051215@mail.ru',
-    subject: req.body.contactFormSubjects,
-    html: `<div>email from: ${req.body.contactFormName}, email: ${req.body.contactFormEmail}
+  try {
+    const data = {
+      from: req.body.contactFormEmail,
+      to: '82051215@mail.ru',
+      subject: req.body.contactFormSubjects,
+      html: `<div>email from: ${req.body.contactFormName}, email: ${req.body.contactFormEmail}
     <hr>message: ${req.body.contactFormMessage}</div>`,
-  };
+    };
 
-  await mailer.sendMail('', data);
-  res.status(200).json({message: 'message sent successfully.'});
-  return next(null);
+    await mailer.sendMail('', data);
+    res.status(200).json({message: 'message sent successfully.'});
+    return next(null);
+  } catch (error) {
+    return next(error);
+  }
 };
 
 module.exports.getUsers = async (req, res, next) => {
-  const users = await User.find();
+  try {
+    const users = await User.find();
 
-  res.status(200).json(users);
-  return next(null);
+    res.status(200).json(users);
+    return next(null);
+  } catch (error) {
+    return next(error);
+  }
 };
 
 module.exports.updatePermission = async (req, res, next) => {
-  const user = await User.findOneAndUpdate(
-    { _id: req.body._id },
-    { type: req.body.type },
-    { new: true }
-  );
+  try {
+    const user = await User.findOneAndUpdate(
+      { _id: req.body._id },
+      { type: req.body.type },
+      { new: true }
+    );
 
-  res.status(200).json(user);
-  return next(null);
+    res.status(200).json(user);
+    return next(null);
+  } catch (error) {
+    return next(error);
+  }
 };
--- packages/server/controllers/stat.js
+++ packages/server/controllers/stat.js
@@ -2,72 +2,92 @@ const StatDataset = require('../models/statDataset.model');
 const { DatasetValidationError, DatasetNotFoundError } = require('../errors/DatasetErrors');
 
 module.exports.addDataset = async (req, res, next) => {
-  if (!req.body.dataset.length) {
-    return next(new DatasetValidationError('Dataset can\'t be empty.'));
-  }
-  req.body.dataset.forEach((item) => {
-    if (typeof item !== "number") {
-      return next(new DatasetValidationError('Dataset can contain only NUMBER.'));
+  try {
+    if (!req.body.dataset.length) {
+      return next(new DatasetValidationError('Dataset can\'t be empty.'));
     }
-  });
-  const dataset = await new StatDataset({
-    data: req.body.dataset,
-    course: req.body.courseId,
-    user: req.user._id
-  }).save();
+    req.body.dataset.forEach((item) => {
+      if (typeof item !== "number") {
+        return next(new DatasetValidationError('Dataset can contain only NUMBER.'));
+      }
+    });
+    const dataset = await new StatDataset({
+      data: req.body.dataset,
+      course: req.body.courseId,
+      user: req.user._id
+    }).save();
 
-  res.status(201).json({
-    dataset,
-    message: `Dataset ${dataset.data} added successfully. You can use this dataset later.`
-  });
-  return next(null);
+    res.status(201).json({
+      dataset,
+      message: `Dataset ${dataset.data} added successfully. You can use this dataset later.`
+    });
+    return next(null);
+  } catch (error) {
+    return next(error);
+  }
 };
 
-module.exports.getAll = async (req, res) => {
-  const dataSet = await StatDataset.find({
-    user: req.user._id,
-    course: req.query.courseId,
-  });
+module.exports.getAll = async (req, res, next) => {
+  try {
+    const dataSet = await StatDataset.find({
+      user: req.user._id,
+      course: req.query.courseId,
+    });
 
-  res.status(200).json(dataSet);
-  return next(null);
+    res.status(200).json(dataSet);
+    return next(null);
+  } catch (error) {
+    return next(error);
+  }
 };
 
 module.exports.getSingle = async (req, res, next) => {
-  const dataSet = await StatDataset.findById(req.params.id);
+  try {
+    const dataSet = await StatDataset.findById(req.params.id);
 
-  if (!dataSet) {
-    return next(new DatasetNotFoundError(req.params.id));
+    if (!dataSet) {
+      return next(new DatasetNotFoundError(req.params.id));
+    }
+    res.status(200).json(dataSet);
+    return next(null);
+  } catch (error) {
+    return next(error);
   }
-  res.status(200).json(dataSet);
-  return next(null);
 };
 
 module.exports.update = async (req, res, next) => {
-  if (req.body.dataset.length === 0) {
-    return next(new DatasetValidationError('Dataset can\'t be empty.'));
-  }
-  req.body.dataset.forEach((item) => {
-    if (typeof item !== "number") {
-      return next(new DatasetValidationError('Dataset can contain only NUMBER.'));
+  try {
+    if (req.body.dataset.length === 0) {
+      return next(new DatasetValidationError('Dataset can\'t be empty.'));
     }
-  });
-  req.body.dataset.map((item) => Number(item));
-  const newDataset = await StatDataset.findOneAndUpdate(
-    { _id: req.params.id },
-    { data: req.body.dataset },
-    { new: true }
-  );
+    req.body.dataset.forEach((item) => {
+      if (typeof item !== "number") {
+        return next(new DatasetValidationError('Dataset can contain only NUMBER.'));
+      }
+    });
+    req.body.dataset.map((item) => Number(item));
+    const newDataset = await StatDataset.findOneAndUpdate(
+      { _id: req.params.id },
+      { data: req.body.dataset },
+      { new: true }
+    );
 
-  res.status(200).json(newDataset);
-  return next(null);
+    res.status(200).json(newDataset);
+    return next(null);
+  } catch (error) {
+    return next(error);
+  }
 };
 
 module.exports.remove = async (req, res, next) => {
-  const dataset = await StatDataset.findOneAndRemove({
-    _id: req.params.id
-  });
+  try {
+    const dataset = await StatDataset.findOneAndRemove({
+      _id: req.params.id
+    });
 
-  res.status(204).json(dataset);
-  return next(null);
+    res.status(204).json(dataset);
+    return next(null);
+  } catch (error) {
+    return next(error);
+  }
 };
