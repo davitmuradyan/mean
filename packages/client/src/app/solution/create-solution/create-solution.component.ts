709777aab30ce40cf2df600d34fbb0e9595e1400
davitmuradyan <davitmuradyan5@gmail.com>
2019-05-20 00:54:22 +0400

    fix create solution bug, when there is no course

--- packages/client/src/app/solution/create-solution/create-solution.component.ts
+++ packages/client/src/app/solution/create-solution/create-solution.component.ts
@@ -18,6 +18,7 @@ export class CreateSolutionComponent implements OnInit, OnDestroy {
   courseSub$: Subscription;
   solCreateSub$: Subscription;
   editorOptions = {theme: 'vs-light', language: 'typescript'};
+  loader = true;
   @ViewChild('alert') alert: ElementRef;
 
   constructor(private courseService: CoursesService, private solutionService: SolutionService) {  }
@@ -26,17 +27,23 @@ export class CreateSolutionComponent implements OnInit, OnDestroy {
     this.courseSub$ = this.courseService.fetch()
       .subscribe(data => {
         this.courses = data.courses;
-        this.form = new FormGroup({
-          courseName: new FormControl(this.courses[0]._id),
-          problem: new FormControl(null, [Validators.required]),
-          comments: new FormControl(null),
-          functionName: new FormControl(null, [Validators.required]),
-          numberOfInputs: new FormControl(null, [Validators.required]),
-          parameters: new FormControl(null, [Validators.required]),
-          testCaseInput: new FormControl(null, [Validators.required]),
-          testCaseOutput: new FormControl(null, [Validators.required]),
-          solution: new FormControl(null, [Validators.required]),
-        });
+        if (data.courses.length) {
+          this.form = new FormGroup({
+            courseName: new FormControl(this.courses[0]._id),
+            problem: new FormControl(null, [Validators.required]),
+            comments: new FormControl(null),
+            functionName: new FormControl(null, [Validators.required]),
+            numberOfInputs: new FormControl(null, [Validators.required]),
+            parameters: new FormControl(null, [Validators.required]),
+            testCaseInput: new FormControl(null, [Validators.required]),
+            testCaseOutput: new FormControl(null, [Validators.required]),
+            solution: new FormControl(null, [Validators.required]),
+          });
+        } else {
+          this.loader = false;
+          this.alertClass = 'danger';
+          this.message = 'No available courses';
+        }
       }, error => console.log(error));
   }
 
